74923de41c2c96b96fb89459da2101185d13c807
soaxic <j@redzer.be>
2025-02-13 16:29:20 -0800

    added text_node_to_html_node function to textnode.py. added unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,6 +19,20 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is another text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
         
+    def test_to_html_node(self):
+        node = TextNode("This is bold text!", TextType.BOLD)
+        testcase = "<b>This is bold text!</b>"
+        self.assertEqual(text_node_to_html_node(node).to_html(), testcase)
+    
+    def test_img_to_html(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.somewebsite.com/someimage.jpg")
+        testcase = '<img src="https://www.somewebsite.com/someimage.jpg" alt="This is an image"></img>'
+        self.assertEqual(text_node_to_html_node(node).to_html(), testcase)
+
+    def test_link_to_html(self):
+        node = TextNode("Click here!", TextType.LINK, "https://www.somewebsite.com/")
+        testcase = '<a href="https://www.somewebsite.com/">Click here!</a>'
+        self.assertEqual(text_node_to_html_node(node).to_html(), testcase)
 
 if __name__ == "__main__":
     unittest.main()
