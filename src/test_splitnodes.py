fb98a27a3220ff0ecea3da7736a6cc83c02824c2
soaxic <j@redzer.be>
2025-02-17 14:56:51 -0800

    added unit tests

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -7,5 +7,14 @@ class TestSplitNodes(unittest.TestCase):
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
             [TextNode("This is text with a ", TextType.TEXT, None), TextNode("bolded", TextType.BOLD, None), TextNode(" section", TextType.TEXT, None)],
-            new_nodes
+            new_nodes,
+        )
+
+    def test_double_split(self):
+        node = TextNode("This is text with *italic* and **bold** sections!", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+        self.assertListEqual(
+            [TextNode("This is text with ", TextType.TEXT, None), TextNode("italic", TextType.ITALIC, None), TextNode(" and ", TextType.TEXT, None), TextNode("bold", TextType.BOLD, None), TextNode(" sections!", TextType.TEXT, None)],
+            new_nodes,
         )
