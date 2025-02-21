d775609ad8ffceceeca59a04d6b18d62bc70a032
soaxic <j@redzer.be>
2025-02-21 15:54:36 -0800

    finished converter.py. Added unittests.

--- src/converter.py
+++ src/converter.py
@@ -8,41 +8,93 @@ def markdown_to_html_code(markdown):
     nodes = []
     for block in blocks:
         blocktype = block_to_blocktype(block)
-        nodes.append(block_to_htmlnodes(block, blocktype))
+        nodes.extend(block_to_htmlnodes(block, blocktype))
     return ParentNode("div", nodes)
 
 def block_to_htmlnodes(block, blocktype):
     match blocktype:
         case BlockType.PARAGRAPH:
-            children = get_children(block)
-            return ParentNode("p", children)
+            #This should split by line break and wrap each section in p
+            children = unwrap_paragraph(block)
+            return children
         case BlockType.HEADING:
-            heading_check = list(block[:6])
             heading_level = 0
-            for letter in heading_check:
+            for letter in block[:6]:
                 if letter == "#":
                     heading_level += 1
                 else:
                     break
             children = get_children(block[heading_level+1:])
-            return ParentNode(f"h{heading_level}", children)
+            return [ParentNode(f"h{heading_level}", children)]
         case BlockType.CODE:
-            return LeafNode("code", block[4:-4])
+            children = get_code_children(block[4:-4])
+            return [ParentNode("pre", children)]
         case BlockType.QUOTE:
-            #TODO - Replace this with a recursive call that sends the lines of the block back through as blocktype.paragraph
-            #lines = block.split("\n")
-            #children = []
-            #for i in range(len(lines)):
-            #    lines[i] = lines[i][1:]
-            #    children.extend(get_children(lines[i]))
-            #for child in children:
-            #   child.tag = "p"
-            #print(children)
-            #return ParentNode("blockquote", children)
+            lines = block.split("\n")
+            children = []
+            for i in range(len(lines)):
+                lines[i] = lines[i][1:]
+                children.extend(block_to_htmlnodes(lines[i], BlockType.PARAGRAPH))
+            return [ParentNode("blockquote", children)]
+        case BlockType.UNORDERED_LIST:
+            children = get_unordered_list_children(block)
+            return [ParentNode("ul", children)]
+        case BlockType.ORDERED_LIST:
+            children = get_ordered_list_children(block)
+            return [ParentNode("ol", children)]
+        
+def unwrap_paragraph(block):
+    lines = block.split("\n")
+    children = []
+    for line in lines:
+        textnodes = []
+        textnodes.extend(text_to_textnodes(line))
+        htmlnodes = []
+        for node in textnodes:
+            htmlnodes.append(text_node_to_html_node(node))
+        children.append(ParentNode("p", htmlnodes))
+    return children
 
 def get_children(block):
     textnodes = text_to_textnodes(block)
     children = []
     for node in textnodes:
         children.append(text_node_to_html_node(node))
+    return children
+
+def get_unordered_list_children(lines):
+    lines = lines.split("\n")
+    children = []
+    for li in lines:
+        textnodes = []
+        textnodes.extend(text_to_textnodes(li[2:]))
+        htmlnodes = []
+        for node in textnodes:
+            htmlnodes.append(text_node_to_html_node(node))
+        children.append(ParentNode("li", htmlnodes))
+    return children
+
+def get_ordered_list_children(lines):
+    lines = lines.split("\n")
+    children = []
+    for i, li in enumerate(lines, 1):
+        prefix_length = len(str(i)) + 2
+        textnodes = []
+        textnodes.extend(text_to_textnodes(li[prefix_length:]))
+        htmlnodes = []
+        for node in textnodes:
+            htmlnodes.append(text_node_to_html_node(node))
+        children.append(ParentNode("li", htmlnodes))
+    return children
+
+def get_code_children(lines):
+    lines = lines.split("\n")
+    children = []
+    for li in lines:
+        textnodes = []
+        textnodes.extend(text_to_textnodes(li))
+        htmlnodes = []
+        for node in textnodes:
+            htmlnodes.append(text_node_to_html_node(node))
+        children.append(ParentNode("code", htmlnodes))
     return children
--- src/test_converter.py
+++ src/test_converter.py
@@ -42,9 +42,56 @@ Here's a picture of a cat:
 
 And a link to my [favorite website](https://www.google.com/)
 """
-        markdown = """
->lol
->quote
->time"""
         pn = markdown_to_html_code(markdown)
-        print(pn.to_html())
+        testcase = "ParentNode(div, [ParentNode(h1, [LeafNode(None, This is a pretty long document that starts with a header!, None)], None), ParentNode(h3, [LeafNode(None, It even has a smaller header!, None)], None), ParentNode(p, [LeafNode(None, It's got some paragraphs, None)], None), ParentNode(p, [LeafNode(None, full of text, None)], None), ParentNode(p, [LeafNode(None, and meaningless information, None)], None), ParentNode(pre, [ParentNode(code, [LeafNode(None, It also has some code blocks., None)], None), ParentNode(code, [LeafNode(None, With code! Who could've guessed?, None)], None), ParentNode(code, [LeafNode(None, print(\"Hello world!\"), None)], None)], None), ParentNode(blockquote, [ParentNode(p, [LeafNode(None, God save the Queen, None)], None), ParentNode(p, [LeafNode(None, A quote from John Madden, None)], None), ParentNode(p, [LeafNode(None, Probably..., None)], None)], None), ParentNode(p, [LeafNode(None, A list of my , None), LeafNode(b, favorite, None), LeafNode(None,  things:, None)], None), ParentNode(ol, [ParentNode(li, [LeafNode(None, My cat, None)], None), ParentNode(li, [LeafNode(None, Pizza, None)], None), ParentNode(li, [LeafNode(None, Ordered lists, None)], None)], None), ParentNode(p, [LeafNode(i, Some, None), LeafNode(None,  things I don't like:, None)], None), ParentNode(ul, [ParentNode(li, [LeafNode(None, Bad drivers, None)], None), ParentNode(li, [LeafNode(None, Cold coffee, None)], None), ParentNode(li, [LeafNode(None, Unordered lists with varied flags, None)], None), ParentNode(li, [LeafNode(None, Readable code, None)], None)], None), ParentNode(p, [LeafNode(None, Here's a picture of a cat:, None)], None), ParentNode(p, [LeafNode(img, , {'src': 'https://catpictures.org/areallycutecat.png', 'alt': 'a picture of a cat'})], None), ParentNode(p, [LeafNode(None, And a link to my , None), LeafNode(a, favorite website, {'href': 'https://www.google.com/'})], None)], None)"
+        self.assertEqual(testcase, str(pn))
+
+    def test_heading(self):
+        markdown = """
+### Heading 3
+
+#### Heading 4
+
+# Heading 1"""
+        pn = markdown_to_html_code(markdown)
+        testcase = "ParentNode(div, [ParentNode(h3, [LeafNode(None, Heading 3, None)], None), ParentNode(h4, [LeafNode(None, Heading 4, None)], None), ParentNode(h1, [LeafNode(None, Heading 1, None)], None)], None)"
+        self.assertEqual(testcase, str(pn))
+
+    def test_code(self):
+        markdown = """```
+code is here
+and here!
+```"""
+        pn = markdown_to_html_code(markdown)
+        testcase = "ParentNode(div, [ParentNode(pre, [ParentNode(code, [LeafNode(None, code is here, None)], None), ParentNode(code, [LeafNode(None, and here!, None)], None)], None)], None)"
+        self.assertEqual(testcase, str(pn))
+
+    def test_quote(self):
+        markdown = """
+>This
+>Is
+>A
+>Quote!"""
+        pn = markdown_to_html_code(markdown)
+        testcase = "ParentNode(div, [ParentNode(blockquote, [ParentNode(p, [LeafNode(None, This, None)], None), ParentNode(p, [LeafNode(None, Is, None)], None), ParentNode(p, [LeafNode(None, A, None)], None), ParentNode(p, [LeafNode(None, Quote!, None)], None)], None)], None)"
+        self.assertEqual(testcase, str(pn))
+
+    def test_ul(self):
+        markdown = """
+* This
+- Is
+* An unordered
+- List!"""
+        pn = markdown_to_html_code(markdown)
+        testcase = "ParentNode(div, [ParentNode(ul, [ParentNode(li, [LeafNode(None, This, None)], None), ParentNode(li, [LeafNode(None, Is, None)], None), ParentNode(li, [LeafNode(None, An unordered, None)], None), ParentNode(li, [LeafNode(None, List!, None)], None)], None)], None)"
+        self.assertEqual(testcase, str(pn))
+
+    def test_ol(self):
+        markdown = """
+1. One!
+2. Two!
+3. Three!
+"""
+        pn = markdown_to_html_code(markdown)
+        testcase = "ParentNode(div, [ParentNode(ol, [ParentNode(li, [LeafNode(None, One!, None)], None), ParentNode(li, [LeafNode(None, Two!, None)], None), ParentNode(li, [LeafNode(None, Three!, None)], None)], None)], None)"
+        self.assertEqual(testcase, str(pn))
